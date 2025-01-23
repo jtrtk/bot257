9f0e7dc8c4d90e0df2dfe13917a3eeb73979432d
jtrtk <j.trytek685@kodland.team>
2025-01-23 18:51:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='-', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -12,14 +12,24 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Cześć, jestem bot {bot.user}!')
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
 
 @bot.command()
-async def heh(ctx, count_heh = 10):
+async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
-async def chat(ctx):
-    await ctx.send("chatchatchatchatchatchatchatchatchatchat")
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
-bot.run("")
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+bot.run("token")
