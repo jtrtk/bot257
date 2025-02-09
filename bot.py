c10fc7bcca55367eba2603a4a4cd2dac9576eb0c
jtrtk <j.trytek685@kodland.team>
2025-02-09 14:47:49 +0100

    Update bot.py
    bot update

--- bot.py
+++ bot.py
@@ -18,18 +18,4 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
-
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
-
 bot.run("token")
